99f892c1cd9a5569618a2ac58ddb79c638b142e1
Jordi Pradel <jordi.pradel@agilogy.com>
2022-11-11 11:40:02 +0100

    Use conversion String => Parser in JsonParser

--- src/test/scala/JsonParser.scala
+++ src/test/scala/JsonParser.scala
@@ -9,10 +9,10 @@ object JsonParser:
   private val whitespace: Parser[Unit] = (s, position) =>
     Right(() -> (position + s.substring(position).takeWhile(c => whiteSpaceChars.contains(c)).length))
 
-  private val jsonTrue: Parser[JsonBoolean] = token("true").as(JsonBoolean(true))
-  private val jsonFalse: Parser[JsonBoolean] = token("false").as(JsonBoolean(false))
+  private val jsonTrue: Parser[JsonBoolean] = "true".as(JsonBoolean(true))
+  private val jsonFalse: Parser[JsonBoolean] = "false".as(JsonBoolean(false))
 
-  val jsonNull: Parser[JsonNull.type]  = token("null").as(JsonNull)
+  val jsonNull: Parser[JsonNull.type]  = "null".as(JsonNull)
 
   val boolean: Parser[JsonBoolean] = jsonTrue | jsonFalse
 
@@ -24,16 +24,12 @@ object JsonParser:
 
   //noinspection ForwardReference
   val array: Parser[Json] =
-    token("[") **
-      ((json ** (token(",") ** json).repeated).map { case (b, l) => JsonArray(b :: l) } | whitespace.as(JsonArray(List.empty)))
-      ** token("]")
+    "[" ** whitespace ** json.separatedBy(",").map(JsonArray.apply) ** token("]")
 
   //noinspection ForwardReference
   val member: Parser[(String, Json)] = string.map(_.value) ** token(":") ** json
 
   val obj: Parser[Json] =
-    token("{") **
-      ((member ** (token(",") ** member).repeated).map { case (b, l) => JsonObject((b :: l).toMap) } | whitespace.as(JsonObject(Map.empty)))
-      ** token("}")
+    "{" ** member.separatedBy(",").map(members => JsonObject(members.toMap)) ** "}"
 
   val json: Parser[Json] = boolean | string | number | jsonNull | array | obj
